cb28893bbe3c2bb130463fa347f68c852586f0df
sam <sampurna.chapagainnn@gmail.com>
2024-02-25 08:19:05 +0000

    create blog urls to perform crud and search operation

---
+++ blogs/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('blog-list/', views.blogList, name='blog-list'),
+    path('blog-detail/<str:pk>/', views.blogDetail, name='blog-detail'),
+    path('blog-create/', views.blogCreate, name='blog-create'),
+    path('blog-update/<str:pk>/', views.blogUpdate, name='blog-update'),
+    path('blog-delete/<str:pk>/', views.blogDelete, name='blog-delete'),
+
+    path('search/', views.blogSearch, name='search-blog')
+
+]
