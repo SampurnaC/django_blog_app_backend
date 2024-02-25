f032fa572d2b522943b3e8123929f56db77a2785
sam <sampurna.chapagainnn@gmail.com>
2024-02-25 08:17:41 +0000

    create blog model with fields

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField()
+    image = models.ImageField(upload_to='uploads/images', null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
