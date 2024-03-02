33ed6bc2e65faf61ea833b300e37570ea1cc7bfe
sam <sampurna.chapagainnn@gmail.com>
2024-03-02 08:58:45 +0000

    add Category model and add association between category and blog model

--- blogs_api/models.py
+++ blogs_api/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "categories"
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Blog(models.Model):
     name = models.CharField(max_length=150, null=False, blank=False)
     description = models.TextField()
-    image = models.ImageField(upload_to='uploads/images', null=True, blank=True)
+    image = models.ImageField(upload_to='', null=True, blank=True, default='default.png')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, related_name="category", default=1)
     
     def __str__(self):
         return self.name
