df6f019860e5febde34642fc0406af87339826f4
sam <sampurna.chapagainnn@gmail.com>
2024-03-02 08:57:10 +0000

    serialize category model

--- blogs_api/serializers.py
+++ blogs_api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Blog
+from .models import Blog, Category
 
 class BlogSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source='category.name')
     class Meta:
         model = Blog
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
         fields = '__all__'
