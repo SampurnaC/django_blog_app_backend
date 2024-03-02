4edf7d82a74e1fa260041a249351ce3900d35850
sam <sampurna.chapagainnn@gmail.com>
2024-03-02 08:57:34 +0000

    add categories lists path

--- blogs_api/urls.py
+++ blogs_api/urls.py
@@ -3,12 +3,15 @@ from . import views
 
 urlpatterns = [
     path('blog-list/', views.blogList, name='blog-list'),
-    path('blog-lists/', views.blogLists, name='blog-lists'),
+    # path('blog-lists/', views.blogLists, name='blog-lists'),
     path('blog-detail/<str:pk>/', views.blogDetail, name='blog-detail'),
     path('blog-create/', views.blogCreate, name='blog-create'),
     path('blog-update/<str:pk>/', views.blogUpdate, name='blog-update'),
     path('blog-delete/<str:pk>/', views.blogDelete, name='blog-delete'),
 
-    path('search/', views.blogSearch, name='search-blog')
+    path('search/', views.blogSearch, name='search-blog'),
+
+
+    path('category-lists/', views.categoryLists, name='category-lists'),
 
 ]
