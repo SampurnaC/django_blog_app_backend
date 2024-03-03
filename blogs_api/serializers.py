69d0d3cb855a9c84c8038c20fb9f56b4834684dc
sam <sampurna.chapagainnn@gmail.com>
2024-03-03 11:16:15 +0000

    use to_representation method to display category name instead of id

--- blogs_api/serializers.py
+++ blogs_api/serializers.py
@@ -2,11 +2,19 @@ from rest_framework import serializers
 from .models import Blog, Category
 
 class BlogSerializer(serializers.ModelSerializer):
-    category = serializers.CharField(source='category.name')
+    # category = serializers.CharField(source='category.id', read_only=False)
+    # category = serializers.CharField(many=False, read_only=False)
+    # category_name = serializers.CharField(source="category.name", read_only=True)
     class Meta:
         model = Blog
-        fields = '__all__'
-
+        fields = ['name','description', 'category']
+    def to_representation(self, obj):
+        return {
+            "name": obj.name,
+            "description": obj.description,
+            "category": obj.category.name
+        }
+       
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
