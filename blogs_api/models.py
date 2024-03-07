92b03f2c6957d89a84bd44ed06dc4f3dd7ec0a09
sam <sampurna.chapagainnn@gmail.com>
2024-03-07 11:04:23 +0000

    add artist, url fields to blog model

--- blogs_api/models.py
+++ blogs_api/models.py
@@ -6,15 +6,20 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "categories"
     name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Blog(models.Model):
-    name = models.CharField(max_length=150, null=False, blank=False)
+    name = models.CharField(max_length=150, null=True, blank=True)
+    artist = models.CharField(max_length=500, null=True, blank=True)
+    url = models.URLField(max_length=500, null=True, blank=True) 
     description = models.TextField()
-    image = models.ImageField(upload_to='', null=True, blank=True, default='default.png')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False, related_name="category", default=1)
-    
+    image = models.ImageField(upload_to='', null=True, blank=True, default='/default.jpg')
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category", db_constraint=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category", default='django')
+
+   
     def __str__(self):
         return self.name
